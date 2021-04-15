0b0dacddecffa87114e4d72456026063a1731461
nevillejodie <jlneville92@gmail.com>
2021-04-15 20:21:18 +0100

    made svg components, linked them, need to look at css

---
+++ src/components/Contact/EmailLogo/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const EmailLogo = () => {
+    return (
+        <div>
+            <svg width="140" height="125" viewBox="0 0 140 125" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d)">
+<path d="M117.071 10H22.93C15.7999 10 10 15.7999 10 22.9291V102.008C10 109.137 15.7999 114.938 22.93 114.938H117.071C124.2 114.938 130 109.137 130 102.008V22.9291C130 15.7999 124.2 10 117.071 10ZM117.071 17.0312C117.364 17.0312 117.651 17.0532 117.933 17.0953L74.1711 60.8575C71.8713 63.1573 68.1287 63.1573 65.8289 60.8575L22.0667 17.0953C22.3486 17.0532 22.6361 17.0312 22.93 17.0312H117.071ZM122.969 102.008C122.969 105.261 120.323 107.906 117.071 107.906H22.93C19.6771 107.906 17.0312 105.261 17.0312 102.008V22.93C17.0312 22.6361 17.0532 22.3486 17.0944 22.0667L60.8575 65.8289C63.378 68.3502 66.6885 69.61 70 69.61C73.3115 69.61 76.622 68.3502 79.1425 65.8289L122.906 22.0667C122.947 22.3486 122.969 22.637 122.969 22.93V102.008Z" fill="#59C7F6"/>
+</g>
+<defs>
+<filter id="filter0_d" x="0" y="0" width="140" height="124.938" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+<feOffset/>
+<feGaussianBlur stdDeviation="5"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0.34902 0 0 0 0 0.780392 0 0 0 0 0.964706 0 0 0 1 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+</filter>
+</defs>
+</svg>
+        </div>
+    )
+}
+
+export default EmailLogo
