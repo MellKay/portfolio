fce6be1fc1235d2866baf902cce1c19b5bac2035
leckzilla <leckier@hotmail.com>
2021-04-15 19:10:29 +0100

    started on profile

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,14 @@
 import "./about.css";
+import self from "../../assets/self.jpg"
+
+// Use whatever image, SoC avatar for placeholder
 
 function About() {
   return (
     <div className="about">
-      <h1>my About Page</h1>
+    <img className="self" src={self} alt="me" />
+      <h1 className="text">About Me</h1>
+      <p className="text">Blah blah blah Mell is awesome blah blah blah</p>
     </div>
   );
 }
