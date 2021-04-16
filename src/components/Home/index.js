0044718d97ded5b9581d73311f9b1656d781f08b
Mell <mellkayrusson@gmail.com>
2021-04-16 11:05:09 +0100

    not quite finished

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,21 +3,15 @@ import "./home.css";
 function Home() {
   return (
     <div className="home">
-      <h1>my HomePage</h1>
+      <h1>Mell Russon</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-        qui officia deserunt mollit anim id est laborum.
+        Welcome reader, to my portfolio! <br />
+        Here you can find a lovely page where I talk about myself, see the
+        journey I've been on, and find out how I went from knowing nothing, to
+        being a full stack developer in just 4 months. You can also have a look
+        at the finest selection of projects that I've been a part of, from a
+        Twitter bot to a full-stack community app! And there's also a contact
+        tab, for if you ever want to contact me!
       </p>
     </div>
   );
