a07bba9e04499e036f06b6f5e10f51cfa80dac1a
Mell <mellkayrusson@gmail.com>
2021-04-23 11:39:47 +0100

    very janky first version of the grid

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,6 +3,7 @@ import "./projects.css";
 import img1 from "../../assets/bnaHome.png";
 import img2 from "../../assets/ani-matehome.png";
 import img3 from "../../assets/twitterBot.PNG";
+import { useState } from "react";
 
 function Projects() {
   // 9 containers with thumbnail images
@@ -14,6 +15,8 @@ function Projects() {
 
   const projects = [
     {
+      id: 0,
+      title: "Bootcamper Community App",
       img: img1,
       description:
         "This was the result of our final project at the School of Code. We had 4 weeks to design and build a full stack community app for bootcampers. It has several features including a bootcamper list where you can see all bootcampers, past and present. It has a companies list where new bootcampers can see where previous bootcampers are working so they can get a good idea of where they can go and it has a list of relevent events in the area pulled from the meetup API, as well as many other features.",
@@ -21,12 +24,16 @@ function Projects() {
       liveSite: "https://bootcamper-network.netlify.app/",
     },
     {
+      id: 1,
+      title: "Ani-Mate",
       img: img2,
       description:
         "This is Ani-Mate, a month long project made by a small group for the Code First Girls: Hack from Home project. The idea behind this project was helping take care of yourself using a small virtual pet. Users would click the hunger button when they eat to feed Ani, the health button when they shower or sleep, and the happiness button when they do anything that makes them happy! We felt this would encourage remembering to take care of yourself, especially given that this was built during the lockdown when many people were having a hard time doing so.",
       repo: "https://github.com/MellKay/Hack-From-Home",
     },
     {
+      id: 2,
+      title: "Bootcamper Twitter Bot",
       img: img3,
       description:
         "This is a twitter bot that was created as part of our final project at School of Code, but deserves its own slot on my portfolio as it's something I'm very proud of making. It looks for tweets with hashtags such as #SchoolOfCode and retweets them to boost their numbers. It also retweets everything tweeted by @theSchoolOfCode. It was originally created so we could have a twitter feed on the homepage of our app that only showed specific and relevant tweets, without having to go and manually retweet every single one. It still recieves updates, particularly on SoC demo days to show support for the bootcampers.",
@@ -35,9 +42,34 @@ function Projects() {
     },
   ];
 
+  const [currentProject, setCurrentProject] = useState(projects[0]);
   return (
     <div className="projects">
       <h1>my projects</h1>
+      <div className="container">
+        <div className="grid-container">
+          {projects.map((item) => (
+            <div className="grid-item">
+              {/* on click of this image, setCurrentProject to be whatever the related project is */}
+              <img
+                src={item.img}
+                className="thumbnail"
+                onClick={() => setCurrentProject(projects[item.id])}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="project-display">
+          <h2>{currentProject.title}</h2>
+          <img src={currentProject.img} className="display-img" />
+          <p>{currentProject.description}</p>
+          {currentProject.liveSite && (
+            <a href={currentProject.liveSite}>Live Hosted Site </a>
+          )}
+          <br />
+          {currentProject.repo && <a href={currentProject.repo}>Github Repo</a>}
+        </div>
+      </div>
     </div>
   );
 }
